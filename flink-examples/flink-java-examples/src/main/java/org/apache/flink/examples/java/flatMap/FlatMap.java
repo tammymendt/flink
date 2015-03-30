3d6f5717350f953ede993e9da47ac7c2178d8ad3
tammymendt <tammymendt@gmail.com>
2015-03-30 11:38:04 +0200

    Adding simple FlatMap task to test how the statistics collection is working

---
+++ flink-examples/flink-java-examples/src/main/java/org/apache/flink/examples/java/flatMap/FlatMap.java
@@ -0,0 +1,31 @@
+package org.apache.flink.examples.java.flatMap;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple1;
+import org.apache.flink.util.Collector;
+
+/**
+ * Created by tamara on 26.03.15.
+ */
+public class FlatMap {
+
+    // *************************************************************************
+    //     PROGRAM
+    // *************************************************************************
+
+    public static void main(String[] args) throws Exception {
+
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        env.fromElements(1,2,34,3).flatMap(new FlatMapFunction<Integer, Tuple1<Integer>>() {
+
+            @Override
+            public void flatMap(Integer value, Collector<Tuple1<Integer>> out) throws Exception {
+                out.collect(new Tuple1(value*200));
+            }
+        }).collectKeyStatistics(new String[]{"f0"}).print();
+
+        // execute program
+        env.execute("Simple Flat Map Example");
+    }
+}
